b47d6134c8691909d3911e32a5e2dd0dbf0ccd83
Alexander Urizar <urizaraj@gmail.com>
2018-03-03 19:52:53 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  def slug
+    self.name.downcase.scan(/[a-z0-9]+/).join('-')
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find { |e| e.slug == slug }
+  end
 end
